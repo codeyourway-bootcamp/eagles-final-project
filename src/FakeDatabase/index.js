596bf3d15a0834652726b10206fa677589d00a39
Wellington-Marcio <wkinformatica2013@gmail.com>
2023-09-06 01:15:11 +0100

    Att fakeDatabase.

--- src/FakeDatabase/index.js
+++ src/FakeDatabase/index.js
@@ -37,4 +37,56 @@ export const user_registration = new Promise((resolve, reject) => {
   }, 2000);
 });
 
+export const contacts = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      labels: [
+      "user_id",
+      "contacts_full_name",
+      "contacts_phone",
+      "contacts_email",
+      "contacts_address",
+      "contacts_social_network",
+
+    ],
+      data: {
+      user_id:"00001",
+      contacts_full_name:"Wellington Martins",
+      contacts_phone:"+351932471578",
+      contacts_email:"wellington@eagles.com",
+      contacts_address:"Rua de Santiagos",
+      contacts_social_network:"wellington.com",
+      },
+    {
+      user_id:"00002",
+      contacts_full_name:"Amario",
+      contacts_phone:"+351932678975",
+      contacts_email:"amario@eagles.com",
+      contacts_address:"Rua de Santiagos",
+      contacts_social_network:"amario.com",
+      },
+      {
+      user_id:"00003",
+      contacts_full_name:"Ana Emily",
+      contacts_phone:"+351932472309",
+      contacts_email:"anaemily@eagles.com",
+      contacts_address:"Rua de Santiagos",
+      contacts_social_network:"anaemily.com",
+      },
+      {
+      user_id:"00004",
+      contacts_full_name:"Pedro Lucca",
+      contacts_phone:"+3519324232228",
+      contacts_email:"pedrolucca@eagles.com",
+      contacts_address:"Rua de Santiagos",
+      contacts_social_network:"pedrolucca",
+      },
+      }
+      }
+
+    });
+  }, 2000);
+});
+
+
 
