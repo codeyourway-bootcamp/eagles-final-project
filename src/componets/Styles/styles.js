1ce594a716414a6fdcc880da96eb2c73084bc333
Wellington Martins <96148724+Wellington-Marcio@users.noreply.github.com>
2023-09-07 19:36:59 +0100

    Update styles.js

--- src/componets/Styles/styles.js
+++ src/componets/Styles/styles.js
@@ -1,9 +1,10 @@
 import { styled, createGlobalStyle } from "styled-components"
 
 
-export const GlobalStyle = createGlobalStyle
+export const GlobalStyle = createGlobalStyle'
+ *{
  color:red;
-
+}'
 
 
 
