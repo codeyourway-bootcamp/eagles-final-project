eb6a63fbac265eab526d28e5f779b0898931299d
Wellington-Marcio <wkinformatica2013@gmail.com>
2023-09-17 08:25:35 +0100

    updating

--- src/componets/Styles/styles.js
+++ src/componets/Styles/styles.js
@@ -1,30 +1,28 @@
 import { styled, createGlobalStyle } from "styled-components";
+
 export const GlobalStyle = createGlobalStyle`
 body{
    
-  /* background: rgb(0,0,0);
-    /* background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(29,39,253,1) 50%, rgba(252,176,69,1) 72%, rgba(69,80,252,1) 88%, rgba(0,0,3,1) 99%); */
-    background: rgb(4,4,10);
-    background: linear-gradient(90deg, rgba(4,4,10,1) 0%, rgba(1,8,77,1) 50%, rgba(1,14,173,1) 100%);
+  background: rgb(0,0,0);
+    background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(29,39,253,1) 50%, rgba(252,176,69,1) 72%, rgba(69,80,252,1) 88%, rgba(0,0,3,1) 99%);
     font-family: Arial;
-    
     
 
 }`;
 
 export const H1 = styled.h1`
-  color: #fff;
-  font-size: 50px;
+color: #fff;
+font-size: 50px;
 `;
 
 export const H1s = styled(H1)`
-  color: #fff;
-  font-size: 30px;
+color: #fff;
+font-size: 30px;
 `;
 
 export const Text = styled.p`
-  font-size: 30px;
-  color: #fff;
+font-size: 30px;
+color: #fff;
 `;
 
 export const NavbarWrapper = styled.div`
@@ -39,7 +37,7 @@ export const Logo = styled.img`
   width: 50px;
   height: auto;
   border-radius: 50%;
-  box-shadow: 2px 2px 4px #2d8bba;
+  box-shadow: 2px 2px 4px #2D8BBA;
 `;
 
 export const CompanyName = styled.h1`
@@ -49,7 +47,9 @@ export const CompanyName = styled.h1`
 `;
 
 export const CapaImage = styled.img`
-  width: 250px;
-  height: 450px;
-  box-shadow: 12px 12px 20px black;
+width: 250px;
+height: 450px;
+box-shadow: 12px 12px 20px black;
+
 `;
+
