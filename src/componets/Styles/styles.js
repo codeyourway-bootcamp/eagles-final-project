c04bf56560c2140a2d0d3bdbefc9164c2d56ca20
Wellington Martins <wkinformatica2013@gmail.com>
2023-09-19 20:42:34 +0100

    work in progress

--- src/componets/Styles/styles.js
+++ src/componets/Styles/styles.js
@@ -3,8 +3,8 @@ import { styled, createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 body{
    
-  background: rgb(4,4,10);
-background: linear-gradient(90deg, rgba(4,4,10,1) 0%, rgba(1,8,77,1) 50%, rgba(1,14,173,1) 100%);
+  background: rgb(0,0,0);
+    background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(29,39,253,1) 50%, rgba(252,176,69,1) 72%, rgba(69,80,252,1) 88%, rgba(0,0,3,1) 99%);
     font-family: Arial;
     
 
