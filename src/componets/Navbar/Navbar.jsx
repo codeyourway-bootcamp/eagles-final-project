b87d342bae496e5630d68917a0c9ca721a2d2b0d
Wellington Martins <wkinformatica2013@gmail.com>
2023-09-30 01:13:20 +0100

    ajustesna navbar.

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,6 +1,42 @@
 
+// import React from "react";
+// import { CompanyName, Logo, NavbarWrapper } from "../Styles/styles";
+// import { ButtonNav } from "../Button/Button";
+// import { ContainerNav } from "../Container/Container";
+// import { useLocation, useNavigate } from "react-router-dom";
+
+// export default function Navbar(props) {
+//   const getUrl = useLocation();
+//   const isLogin = getUrl.pathname === "/login";
+//   const isDashboard = getUrl.pathname === "/dashboard";
+//   const isRegister = getUrl.pathname === "/register";
+//   const isHome = getUrl.pathname === "/";
+
+//   const navegation = useNavigate();
+
+//   return (
+//     <NavbarWrapper>
+//       <ContainerNav>
+//         <Logo src="/Img/Logomark.png" alt="Logo da Empresa" />
+//         <CompanyName>QUICKOPINION</CompanyName>
+//       </ContainerNav>
+//       <div>
+//         <ButtonNav
+//           onClick={function () {
+//             navegation("/login");
+//           }}
+//         >
+//           {isLogin ? "Cadastre-se" :  "null"}
+//           {isRegister ? "Login" : "null"}
+//           {isDashboard? "Sair" : "null"}
+//           {isHome? "Login" : "null"}
+//         </ButtonNav>
+//       </div>
+//     </NavbarWrapper>
+//   );
+// };
 import React from "react";
-import { CompanyName, Logo, NavbarWrapper } from "../../componets/Styles/styles";
+import { CompanyName, Logo, NavbarWrapper } from "../Styles/styles";
 import { ButtonNav } from "../Button/Button";
 import { ContainerNav } from "../Container/Container";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -11,6 +47,7 @@ export default function Navbar(props) {
   const isDashboard = getUrl.pathname === "/dashboard";
   const isRegister = getUrl.pathname === "/register";
   const isHome = getUrl.pathname === "/";
+  const isFeedback = getUrl.pathname === "/feedback";
 
   const navegation = useNavigate();
 
@@ -26,10 +63,11 @@ export default function Navbar(props) {
             navegation("/login");
           }}
         >
-          {isLogin ? "Cadastre-se" :  "null"}
-          {isRegister ? "Login" : "null"}
-          {isDashboard? "Sair" : "null"}
-          {isHome? "Login" : "null"}
+          {isLogin ? "Cadastre-se" :  ""}
+          {isRegister ? "Login" : ""}
+          {isDashboard? "Sair" : ""}
+          {isHome? "Login" : " "}
+          {isFeedback? "Sair" : ""}
         </ButtonNav>
       </div>
     </NavbarWrapper>
